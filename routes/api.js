58b454a2025dc07d896e38f30d4eb272bd3f2f4b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-07-31 23:21:38 +0800

    Fixed the problem with images not being found.

--- routes/api.js
+++ routes/api.js
@@ -9,49 +9,49 @@ router.get('/images', (req, res, next) => {
         "albumTitle": "Zeald Family Day",
         "images": [
             {
-                "path": "images/0190713-zeald-fam-5.jpg"
+                "path": "images/20190713-zeald-fam-5.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-23.jpg"
+                "path": "images/20190713-zeald-fam-23.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-26.jpg"
+                "path": "images/20190713-zeald-fam-26.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-28.jpg"
+                "path": "images/20190713-zeald-fam-28.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-57.jpg"
+                "path": "images/20190713-zeald-fam-57.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-100.jpg"
+                "path": "images/20190713-zeald-fam-100.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-118.jpg"
+                "path": "images/20190713-zeald-fam-118.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-126.jpg"
+                "path": "images/20190713-zeald-fam-126.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-128.jpg"
+                "path": "images/20190713-zeald-fam-128.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-171.jpg"
+                "path": "images/20190713-zeald-fam-171.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-178.jpg"
+                "path": "images/20190713-zeald-fam-178.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-180.jpg"
+                "path": "images/20190713-zeald-fam-180.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-189.jpg"
+                "path": "images/20190713-zeald-fam-189.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-190.jpg"
+                "path": "images/20190713-zeald-fam-190.jpg"
             },
             {
-                "path": "images/0190713-zeald-fam-221.jpg"
+                "path": "images/20190713-zeald-fam-221.jpg"
             }
         ]
     });
