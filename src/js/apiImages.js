50f3a9ad68fef5db91ea95afd11409aeaeafe29b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-08-01 23:29:49 +0800

    Installed service worker

--- src/js/apiImages.js
+++ src/js/apiImages.js
@@ -1,3 +1,11 @@
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/js/service-worker.js');
+    });
+}
+
 (() => {
     let photoListSection = document.querySelector('#photo-grid > ul.my-masonry-image-list');
 
@@ -8,11 +16,11 @@
                 let grid = document.createElement('li');
                 grid.classList.add('mdc-image-list__item');
                 grid.classList.add('photo-container');
-                
+
                 let image = document.createElement('img');
-                image.src = '/'+item.path;
+                image.src = '/' + item.path;
                 image.classList.add('mdc-image-list__image');
-                
+
                 let labelContainer = document.createElement('div');
                 labelContainer.classList.add('mdc-image-list__supporting');
                 let label = document.createElement('span');
