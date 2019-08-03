dcc8c32cf4b4bb2774db80cafd52b16d9224ceb8
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-08-03 18:03:06 +0800

    Removed the page version that is driven by API.

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -2,58 +2,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 if (workbox) {
     workbox.precaching.precacheAndRoute([]);
-
-    workbox.routing.registerRoute(
-        /(.*)images(.*)\.(?:png|gif|jpg)/,
-        workbox.strategies.CacheFirst({
-            cacheName: 'images-cache',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                })
-            ]
-        })
-    );
-
-    workbox.routing.registerRoute(
-        /(.*)js(.*)\.(js)/,
-        workbox.strategies.CacheFirst({
-            cacheName: 'js-cache',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                })
-            ]
-        })
-    );
-
-    workbox.routing.registerRoute(
-        /(.*)styles(.*)\.(css)/,
-        workbox.strategies.CacheFirst({
-            cacheName: 'styles-cache',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                })
-            ]
-        })
-    );
-
-    workbox.routing.registerRoute(
-        /(.*)views(.*)\.(html)/,
-        workbox.strategies.CacheFirst({
-            cacheName: 'html-cache',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                })
-            ]
-        })
-    );
 } else {
     console.log(`Boo! Workbox didn't load!`);
 }
