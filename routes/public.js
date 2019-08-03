17e679441c84343487bbdbe76253bccd9bef8a0c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-08-04 03:16:54 +0800

    Created pages for index and created additional routes.

--- routes/public.js
+++ routes/public.js
@@ -4,6 +4,10 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
 router.get('/best-images', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'best-images.html'));
 });
