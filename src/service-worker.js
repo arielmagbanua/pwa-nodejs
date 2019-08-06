dd0029055ddcf0e398da5d2d618b7201eca465de
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-08-07 01:15:35 +0800

    Updated the route caching.

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,27 +7,59 @@ workbox.core.clientsClaim();
 
 workbox.precaching.precacheAndRoute([]);
 
+// cache all google fonts
+workbox.routing.registerRoute(
+    'https://fonts.googleapis.com/(.*)',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24, // bust the cache after 24 hours
+                maxEntries: 30,
+            })
+        ]
+    })
+);
+
 // best images page
 workbox.routing.registerRoute(
     new RegExp('\/best-images$'),
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'route'
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'route',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            })
+        ]
     })
 );
 
 // favorites tweet
 workbox.routing.registerRoute(
     new RegExp('\/favorite-tweets$'),
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'route'
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'route',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            })
+        ]
     })
 );
 
 // cache page index
 workbox.routing.registerRoute(
     new RegExp('\/$'),
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'route'
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'route',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            })
+        ]
     })
 );
 
@@ -35,13 +67,27 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     new RegExp('api\/twitter\/[0-9a-zA-Z_]+\/favorites'),
     new workbox.strategies.NetworkFirst({
-        cacheName: 'api'
+        cacheName: 'api',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            })
+        ]
     })
 );
 
 workbox.routing.registerRoute(
     new RegExp('\/images\/large\/(.+)\.(jpg|jpeg|png|gif)$'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'images-large'
+    new workbox.strategies.CacheFirst({
+        cacheName: 'images-large',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 30, // bust the cache after 30 days
+                maxEntries: 50,
+            })
+        ]
     })
 );
